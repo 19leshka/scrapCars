8c38ae814bb4469f10a7bca7128c2c70e921845e
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-06-22 15:17:47 +0300

    change main file, middleware for db

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import asyncio
 
+from aiogram import Bot, Dispatcher
+
+from bot.handler import register_handlers
 from bs import scrape
-from database.database import main_async
+from core.settings import API_TOKEN, mongo_url
+from db.mongodb import MongoAdapter
+from middleware import MongoMiddleware
+
 
 URL = "https://cars.av.by/filter?price_usd[min]=10000&price_usd[max]=12500&transmission_type=2&body_type[0]=6&body_type[1]=4&engine_type[0]=5&seller_type[0]=1&mileage_km[max]=220000&sort=4"
 
 
 async def main():
-    tasks = []
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher(bot)
+    db = MongoAdapter(mongo_url=mongo_url)
+    dp.middleware.setup(MongoMiddleware(db))
+    register_handlers(dp)
+    session = await bot.get_session()
+    try:
+        await dp.start_polling()
+    finally:
+        await session.close()  # type: ignore
 
-    db = await main_async()
-    task = asyncio.create_task(scrape(URL))
-    tasks.append(task)
 
-    await asyncio.gather(*tasks)
+if __name__ == '__main__':
+    asyncio.run(main())
 
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
