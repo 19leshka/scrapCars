c127dae090baa25031e4594316f7e7a744bca3ef
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-07-10 17:36:53 +0300

    Chat model

--- database/models/chat.py
+++ database/models/chat.py
@@ -1,9 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class BaseChat(BaseModel):
     chat_id: int
+    first_name: str
+    last_name: str
+    username: str
+    type: str
 
 
 class BaseChatCreate(BaseModel):
     chat_id: int
+
+
+class ChatSchema(BaseModel):
+    id: int = Field(...)
+    first_name: str = Field(...)
+    last_name: str = Field(...)
+    username: str = Field(...)
+    type: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'id': 1234567,
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'username': 'John Doe',
+                'type': 'private'
+            }
+        }
