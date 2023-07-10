318afd8fbba5f493111c0bbb4716a4504ad8477b
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-07-10 17:34:56 +0300

    handler for registration

--- app/bot/handler.py
+++ app/bot/handler.py
@@ -1,13 +1,22 @@
+import logging
+
 from aiogram import Dispatcher
 from aiogram.types import Message
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from database.mongodb import MongoAdapter
+from database.crud.chat import create_chat, get_chat_by_id
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-async def registration(message: Message):
-    chat_id = message.chat.id
-    # collection = db.get_collection("mydatabase", "users")
-    # print(collection)
+async def registration(message: Message, client: AsyncIOMotorClient):
+    chat_is = await get_chat_by_id(client, message.chat.id)
+    if not chat_is:
+        chat = await create_chat(client, message.chat)
+        await message.answer(text=f'Добро пожаловать {chat["first_name"]} {chat["last_name"]}.')
+    else:
+        await message.answer(text='Вы уже зарегистрированы.')
 
 
 def register_handlers(dp: Dispatcher):
